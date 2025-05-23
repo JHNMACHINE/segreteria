8b69444ae153534a0557f2ce160e0c0cfb972611
DarknessMemories <davide.scognami2011@gmail.com>
2025-05-23 14:56:33 +0200

    Model

--- src/main/java/com/universita/segreteria/SegreteriaApplication.java
+++ src/main/java/com/universita/segreteria/SegreteriaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SegreteriaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Ciao");
 		SpringApplication.run(SegreteriaApplication.class, args);
 	}
 
