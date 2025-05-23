d1eca4170733856aa2a7967315f653b2f7b5a7fd
jonat <jonathanvecchione05@gmail.com>
2025-05-23 15:07:16 +0200

    first commit

--- src/main/java/com/universita/segreteria/SegreteriaApplication.java
+++ src/main/java/com/universita/segreteria/SegreteriaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SegreteriaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao");
 		SpringApplication.run(SegreteriaApplication.class, args);
 	}
 
