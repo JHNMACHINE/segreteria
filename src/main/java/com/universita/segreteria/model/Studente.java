b3f766c8a6af3aa221013481a916075ab583bd53
jonat <jonathanvecchione05@gmail.com>
2025-05-23 15:13:08 +0200

    first jpa config on models

--- src/main/java/com/universita/segreteria/model/Studente.java
+++ src/main/java/com/universita/segreteria/model/Studente.java
@@ -1,87 +1,43 @@
 package com.universita.segreteria.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Studente
 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String nome, cognome, matricola, residenza;
     private LocalDate dataDiNascita;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "piano_id")
     private PianoDiStudi pianoDiStudi;
+
+    @OneToMany(mappedBy = "studente", cascade = CascadeType.ALL)
     private List<Tassa> tassePagate;
+
+    @OneToMany(mappedBy = "studente", cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<Voto> voti;
+
+    @OneToMany(mappedBy = "studente", cascade = CascadeType.ALL)
     private List<Esame> esami;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getMatricola() {
-        return matricola;
-    }
-
-    public void setMatricola(String matricola) {
-        this.matricola = matricola;
-    }
-
-    public String getResidenza() {
-        return residenza;
-    }
-
-    public void setResidenza(String residenza) {
-        this.residenza = residenza;
-    }
-
-    public LocalDate getDataDiNascita() {
-        return dataDiNascita;
-    }
-
-    public void setDataDiNascita(LocalDate dataDiNascita) {
-        this.dataDiNascita = dataDiNascita;
-    }
-
-    public PianoDiStudi getPianoDiStudi() {
-        return pianoDiStudi;
-    }
-
-    public void setPianoDiStudi(PianoDiStudi pianoDiStudi) {
-        this.pianoDiStudi = pianoDiStudi;
-    }
-
-    public List<Voto> getVoti() {
-        return voti;
-    }
-
-    public void setVoti(List<Voto> voti) {
-        this.voti = voti;
-    }
-
-    public List<Tassa> getTassePagate() {
-        return tassePagate;
-    }
-
-    public void setTassePagate(List<Tassa> tassePagate) {
-        this.tassePagate = tassePagate;
-    }
-
-    public List<Esame> getEsami() {
-        return esami;
-    }
-
-    public void setEsami(List<Esame> esami) {
-        this.esami = esami;
-    }
 }
--- src/main/java/com/universita/segreteria/model/Voto.java
+++ src/main/java/com/universita/segreteria/model/Voto.java
@@ -1,8 +1,30 @@
 package com.universita.segreteria.model;
 
-public class Voto
-{
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Voto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonBackReference
     private Studente studente;
+
+    @ManyToOne
     private Esame esame;
+
     private int voto;
+    private Boolean accettato; // true = accettato, false = rifiutato, null = in attesa
 }
