a9380dfec0def4746c9ed0d67b2499c7ffaca975
DarknessMemories <davide.scognami2011@gmail.com>
2025-06-15 18:34:32 +0200

    Login fix

--- src/main/resources/static/js/auth.js
+++ src/main/resources/static/js/auth.js
@@ -1,6 +1,6 @@
 const AUTH_BASE_URL = "/api/v1/auth";
 
-async function login({ formId, emailId, passwordId, errorId, redirectUrl }) {
+export async function login({ formId, emailId, passwordId, errorId, redirectUrl }) {
   const form = document.getElementById(formId);
   const errorDiv = document.getElementById(errorId);
 
